6e20bb34fecb84bd1d14eb812b392a4e1db55add
Nitin Yadav <nitinyadav3720@gmail.com>
2023-12-17 21:07:04 +0530

    initial

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # yelp-camp
+
+## Run locally
+1. Clone the repo `git clone https://github.com/Nitin-Yadav-1/yelp-camp.git`
+2. run `npm install`
+3. create .env file with MONGO_DB = 'mongodb url', PORT = 'port number', ADMIN_CODE = 'admin code'
+4. run `node app.js`
---
+++ app.js
@@ -0,0 +1,98 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    cookieParser = require("cookie-parser"),
+    LocalStrategy = require("passport-local"),
+    flash        = require("connect-flash"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    session = require("express-session"),
+    seedDB      = require("./seeds"),
+    methodOverride = require("method-override");
+// configure dotenv
+require('dotenv').load();
+
+//requiring routes
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+    
+// assign mongoose promise library and connect to database
+mongoose.Promise = global.Promise;
+
+// ====================================================================================================================
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const databaseUri = process.env.MONGODB_URI || 'mongodb://localhost/yelp_camp';
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(databaseUri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server (optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("yelp-camp").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//     console.log("closed db connection");
+//   }
+// }
+// run().catch(console.dir);
+// ====================================================================================================================
+
+
+
+mongoose.connect(databaseUri, { useMongoClient: true, useUnifiedTopology: true, useNewUrlParser : true })
+      .then(() => console.log(`Database connected`))
+      .catch(err => console.log(`Database connection error: ${err.message}`));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride('_method'));
+app.use(cookieParser('secret'));
+//require moment
+app.locals.moment = require('moment');
+// seedDB(); //seed the database
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   res.locals.success = req.flash('success');
+   res.locals.error = req.flash('error');
+   next();
+});
+
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+app.listen(process.env.PORT, function(){
+   console.log("The YelpCamp Server Has Started on : " + process.env.PORT);
+});
